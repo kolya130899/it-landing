bb9bf088ce1ec8ab16efb7fc085f474fc90d1333
kolya130899 <kolyawote75@gmail.com>
2018-12-25 18:42:26 +0200

    v.1.3

--- js/main.js
+++ js/main.js
@@ -1,59 +1,47 @@
 
 		//DROPDOWN MENU
 $(document).ready(function(){
-	console.log($(document).width());
-	var $menu = $('#nav li');
-	$menu.hide();
+	var width = $(window).width();
+	console.log(width);
+	var menu = $('#nav');
+	menu.hide();
+	var atr = menu.attr('style');
+	console.log(atr);
+
+	$('.my-toggle-btn').on('click', function(){
+		atr = menu.attr('style');
+		console.log(atr);
+		if (atr === 'display: none;') {
+			menu.show('slow');
+			menu.attr('style','display: block;');
+			/*menu.removeAttr('style');
+			*/
+		}
+		else{
+			menu.hide('slow');
+			/*menu.removeAttr('style');
+			menu.attr('style','display: none;');*/
+		}
+	});
+
+
+	/*console.log($(document).width());
+
+	menu.hide();
 	var wind = $(document).width();
-	var attrib = $menu.attr('style');
-	console.log(attrib);
+
 	if (wind >= 1200 ){
 		$('.my-toggle-btn').hover(
 			function(){
-					$menu.stop().slideDown(2000);
+
 				},
 			function(){
-					$menu.stop().slideUp(2000);
+					menu.stop().slideUp(2000);
 				}
 
 		);
 	}
 	else {
-		$('.my-toggle-btn').on('click', function(){
-			if (attrib === 'display: none;') {
-				$menu.removeAttr('style')
-			}
-			else{
-				$menu.attr('style', attrib);
-			}
-		});
-	}
+
+	}*/
 });
-/*$(document).on("click", function (e) {
-   if (!$menu.is(e.target) //if not menu...
-   && $menu.has(e.target).length === 0)//...and not child of menu
-   {
-     $menu.slideUp(400);
-  }
- });*/
-
-/*$('.my-toggle-btn').on("click", function(){
-	if($menu.hasClass('is-hidden')){
-		$menu.removeClass('is-hidden');
-		$menu.toggleClass('is-open');
-	}
-	else{
-		$menu.toggleClass('is-hidden').fadeOut('slow');
-	}
-});
-
-$*/
-
-
-
-
-
-
-
-/*if($menu)
-*/
