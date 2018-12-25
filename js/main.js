4388f5ca32e38190fceeca76baf6aadd14281bc1
kolya130899 <kolyawote75@gmail.com>
2018-12-26 01:26:54 +0200

    v.1.5

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 
 		//DROPDOWN MENU
 $(document).ready(function(){
-	var width = $(window).width();
-	console.log(width);
 	var menu = $('#nav');
 	menu.hide();
 	var atr = menu.attr('style');
-	console.log(atr);
 
 	$('.my-toggle-btn').on('click', function(){
 		atr = menu.attr('style');
-		console.log(atr);
 		if (atr === 'display: none;') {
 			menu.show('slow');
-			menu.attr('style','display: block;');
 		}
 		else{
 			menu.hide('slow');
