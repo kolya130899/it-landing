1c70b346aa2ce1c93b8d121df2dc825a56f631f2
kolya130899 <kolyawote75@gmail.com>
2018-12-26 11:28:00 +0200

    v.2.0

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 
+
+ $('.center').slick({
+   centerMode: true,
+   infinite: true,
+   arrows: false,
+   autoplay: true,
+   autoplaySpeed: 2000,
+   centerPadding: '30px',
+   slidesToShow: 3,
+   adaptiveWidht: true,
+   responsive: [
+     {
+       breakpoint: 768,
+       settings: {
+         arrows: false,
+         centerMode: true,
+         centerPadding: '20px',
+         slidesToShow: 1
+       }
+     },
+     {
+       breakpoint: 480,
+       settings: {
+         arrows: false,
+         centerMode: true,
+         centerPadding: '20px',
+         slidesToShow: 1
+       }
+     }
+   ]
+ });
+
 		//DROPDOWN MENU
 $(document).ready(function(){
 	var menu = $('#nav');
@@ -15,3 +47,4 @@ $(document).ready(function(){
 		}
 	});
 });
+
